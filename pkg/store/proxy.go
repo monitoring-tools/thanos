30f5113f047e393c05bf77713d611fbd2d4bfdb9
Aleskey Sin <leks.sin@gmail.com>
2020-03-04 13:23:53 +0300

    ProxyStore metrics.

--- pkg/store/proxy.go
+++ pkg/store/proxy.go
@@ -56,8 +56,21 @@ type ProxyStore struct {
 	metrics         *proxyStoreMetrics
 }
 
+const WITH_PAYLOAD_LABEL = "with_payload"
+const WITHOUT_PAYLOAD_LABEL = "without_payload"
+
+type firstRecvMetrics struct {
+	withPayload    prometheus.Observer
+	withoutPayload prometheus.Observer
+	frameTimeoutCount   prometheus.Counter
+	queryTimeoutCount   prometheus.Counter
+}
+
 type proxyStoreMetrics struct {
 	emptyStreamResponses prometheus.Counter
+	firstRecvDuration    *prometheus.HistogramVec
+	frameTimeoutRecvCount     *prometheus.CounterVec
+	queryTimeoutRecvCount     *prometheus.CounterVec
 }
 
 func newProxyStoreMetrics(reg prometheus.Registerer) *proxyStoreMetrics {
@@ -68,9 +81,28 @@ func newProxyStoreMetrics(reg prometheus.Registerer) *proxyStoreMetrics {
 		Help: "Total number of empty responses received.",
 	})
 
+	m.firstRecvDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:       "thanos_proxy_time_to_first_byte_seconds",
+		Help:       "Time to get first part data from store.",
+		Buckets: []float64{0.001, 0.01, 0.1, 0.3, 0.6, 1, 3, 6, 9, 20, 30, 60, 90, 120},
+	}, []string{"store", "payload"})
+
+	m.frameTimeoutRecvCount = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "thanos_proxy_frame_timeouted_requests_count",
+		Help: "Frame timeout recv count.",
+	}, []string{"store"})
+
+	m.queryTimeoutRecvCount = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "thanos_proxy_query_timeouted_requests_count",
+		Help: "Query timeout recv count.",
+	}, []string{"store"})
+
 	if reg != nil {
 		reg.MustRegister(
 			m.emptyStreamResponses,
+			m.firstRecvDuration,
+			m.frameTimeoutRecvCount,
+			m.queryTimeoutRecvCount,
 		)
 	}
 	return &m
@@ -280,10 +312,17 @@ func (s *ProxyStore) Series(r *storepb.SeriesRequest, srv storepb.Store_SeriesSe
 				continue
 			}
 
+			metrics := &firstRecvMetrics{
+				withoutPayload:       s.metrics.firstRecvDuration.WithLabelValues(st.Addr(), WITHOUT_PAYLOAD_LABEL),
+				withPayload:          s.metrics.firstRecvDuration.WithLabelValues(st.Addr(), WITH_PAYLOAD_LABEL),
+				frameTimeoutCount:         s.metrics.frameTimeoutRecvCount.WithLabelValues(st.Addr()),
+				queryTimeoutCount:         s.metrics.queryTimeoutRecvCount.WithLabelValues(st.Addr()),
+			}
+
 			// Schedule streamSeriesSet that translates gRPC streamed response
 			// into seriesSet (if series) or respCh if warnings.
 			seriesSet = append(seriesSet, startStreamSeriesSet(seriesCtx, s.logger, closeSeries,
-				wg, sc, respSender, st.String(), !r.PartialResponseDisabled, s.responseTimeout, s.metrics.emptyStreamResponses))
+				wg, sc, respSender, st.String(), !r.PartialResponseDisabled, s.responseTimeout, s.metrics.emptyStreamResponses, metrics))
 		}
 
 		level.Debug(s.logger).Log("msg", strings.Join(storeDebugMsgs, ";"))
@@ -369,6 +408,7 @@ func startStreamSeriesSet(
 	partialResponse bool,
 	responseTimeout time.Duration,
 	emptyStreamResponses prometheus.Counter,
+	metrics *firstRecvMetrics,
 ) *streamSeriesSet {
 	s := &streamSeriesSet{
 		ctx:             ctx,
@@ -397,6 +437,7 @@ func startStreamSeriesSet(
 		rCh := make(chan *recvResponse)
 		done := make(chan struct{})
 		go func() {
+			t0 := time.Now()
 			for {
 				r, err := s.stream.Recv()
 				select {
@@ -404,6 +445,13 @@ func startStreamSeriesSet(
 					close(rCh)
 					return
 				case rCh <- &recvResponse{r: r, err: err}:
+					if numResponses == 0 {
+						if r.Size() > 0 {
+							metrics.withPayload.Observe(time.Since(t0).Seconds())
+						} else {
+							metrics.withoutPayload.Observe(time.Since(t0).Seconds())
+						}
+					}
 				}
 			}
 		}()
@@ -413,9 +461,11 @@ func startStreamSeriesSet(
 			var rr *recvResponse
 			select {
 			case <-ctx.Done():
+				metrics.queryTimeoutCount.Inc()
 				s.handleErr(errors.Wrapf(ctx.Err(), "failed to receive any data from %s", s.name), done)
 				return
 			case <-frameTimeoutCtx.Done():
+				metrics.frameTimeoutCount.Inc()
 				s.handleErr(errors.Wrapf(frameTimeoutCtx.Err(), "failed to receive any data in %s from %s", s.responseTimeout.String(), s.name), done)
 				return
 			case rr = <-rCh:
